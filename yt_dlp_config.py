61c12a78a3172de3116929c41c74b08c9ddf1062
venysssssssssss <shablaur7@hotmail.com>
2024-11-02 13:15:17 -0300

    Refactor yt_dlp_config.py for improved code organization and readability

--- yt_dlp_config.py
+++ yt_dlp_config.py
@@ -1,6 +1,7 @@
-import yt_dlp as youtube_dl
-import discord
 import asyncio
+
+import discord
+import yt_dlp as youtube_dl
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
@@ -13,11 +14,12 @@ ytdl_format_options = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0'
+    'source_address': '0.0.0.0',
 }
 
 ffmpeg_options = {'options': '-vn'}
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -29,10 +31,14 @@ class YTDLSource(discord.PCMVolumeTransformer):
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        data = await loop.run_in_executor(
+            None, lambda: ytdl.extract_info(url, download=not stream)
+        )
 
         if 'entries' in data:
             data = data['entries'][0]
 
         filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+        return cls(
+            discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data
+        )
